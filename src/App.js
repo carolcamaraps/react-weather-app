5d8a49a44ba53ebfac1d847dec64ebabaec7d163
carolcamaraps <carol@carolinacamara.com>
2023-10-23 20:51:47 -0300

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        Project coded by Carolina Câmara as part of the SheCodes React Workshop
+        and is{" "}
+        <a
+          href="https://github.com/carolcamaraps/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 };
