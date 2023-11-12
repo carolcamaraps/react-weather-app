3092de9b828016b113ca096d7bb7bc15ae8ba8c3
carolcamaraps <carol@carolinacamara.com>
2023-11-12 15:12:47 -0300

    started to build the Forecast section

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,17 @@
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
 export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            Thu
-            icon
-            19 10
+            <div className="WeatherForecast-day">Thu</div>
+            <WeatherIcon code="mist-day" size={36} />
+            <div className="WeatherForecast-temperatures">
+              <span className="WeatherForecast-temperature-max">19º</span>
+              <span className="WeatherForecast-temperature-min">10º</span>
+            </div>
           </div>
         </div>
       </div>
